bdf736c34e70b3ffea4c6fb0747c097a6cdec8d3
Nick <73262982+nicholasc0523@users.noreply.github.com>
2024-10-08 16:59:12 -0400

    Update setup_server.sh

--- setup_server.sh
+++ setup_server.sh
@@ -16,7 +16,7 @@ mv ./mc_server.sh "$ASCII_SCRIPT"
 chmod +x "$ASCII_SCRIPT"
 
 # Create cron job to run every 2 minutes
-(crontab -l 2>dev/null; echo "*/2 * * * * $ASCII_SCRIPT") | crontab -
+(crontab -l 2>/dev/null; echo "*/2 * * * * $ASCII_SCRIPT") | crontab -
 
 # Check current crontab entries
 echo "Current crontab entries:"
