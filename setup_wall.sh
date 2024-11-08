255f00dc04f6130d00d741668543bbbdf352d6a6
Nick <73262982+nicholasc0523@users.noreply.github.com>
2024-11-08 18:05:50 -0500

    Update setup_wall.sh

--- setup_wall.sh
+++ setup_wall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Make this script executable
-chmod +x "$0"
-
 # Make new directories for script to be placed
 mkdir -p /.bin/projects
 
@@ -13,7 +10,7 @@ ASCII_SCRIPT="/.bin/projects/load_gddc.sh"
 mv ./load_gddc.sh "$ASCII_SCRIPT"
 
 # Make the ASCII art script executable
-chmod +x "$ASCII_SCRIPT"
+sudo chmod +x "$ASCII_SCRIPT"
 
 # Create cron job to run every 2 minutes
 (crontab -l 2>/dev/null; echo "*/2 * * * * $ASCII_SCRIPT") | crontab -
