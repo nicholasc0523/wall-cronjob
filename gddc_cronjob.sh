5506e90334ca8e78803203268ce64a2b30a1cad6
Nick <73262982+nicholasc0523@users.noreply.github.com>
2024-11-08 18:20:05 -0500

    Update gddc_cronjob.sh

--- gddc_cronjob.sh
+++ gddc_cronjob.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define ASCII_SCRIPT variable
+ASCII_SCRIPT="/.bin/projects/load_gddc.sh"
+
 # Create cron job to run every 2 minutes
 (crontab -l 2>/dev/null; echo "*/2 * * * * $ASCII_SCRIPT") | crontab -
 
