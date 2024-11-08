c88ea5fba24f350ddc01eb6155b4140b9bf24da6
Nick <73262982+nicholasc0523@users.noreply.github.com>
2024-11-08 18:16:39 -0500

    Update gddc_cronjob.sh

--- gddc_cronjob.sh
+++ gddc_cronjob.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Create cron job to run every 2 minutes
+(crontab -l 2>/dev/null; echo "*/2 * * * * $ASCII_SCRIPT") | crontab -
+
+# Check current crontab entries
+echo "Current crontab entries:"
+crontab -l 
